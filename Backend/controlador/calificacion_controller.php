acabaecafaeb8c952dcd54697eff007490097da9
karolayn <karolaynmazo@gmail.com>
2020-09-23 09:10:47 -0500

    Se finalizo el desarrollo, se corrige la clase conversacion, y se inicia el proceso de integracion

--- Backend/controlador/calificacion_controller.php
+++ Backend/controlador/calificacion_controller.php
@@ -2,16 +2,103 @@
 require('../modelo/conversacion.php');
 class Calificacion{
     
-    static function calificar (){
+  function calificar (){
+     
+    //recorrer el arreglo de las conversaciones 
+    $conversacion = new Conversacion();
+    $Listaconversaciones = $conversacion->extraerConversacion();
 
-         //recorrer el arreglo de las conversaciones 
-         $conversaciones = Conversacion:: extraer_conservacion();
+    foreach($Listaconversaciones as $posicion =>$conversacion){
+//----------si abandono la conversacion---------
+      if(sizeof($conversacion->obtenerMensajes()) == 1){
+        $conversacion->sumarPuntaje(-100);
+      }  
+//---------- Numero de mensajes enviados--------------------
+      if(sizeof($conversacion->obtenerMensajes()) <= 5){
+        $conversacion->sumarPuntaje(20);
+      }
+      else{
+        $conversacion->sumarPuntaje(10);
+      }
+//--------Lista de palabras que exclaman un buen servicio
+      $terminarCalificar = false;
+      foreach($conversacion->obtenerMensajes() as $posicion => $palabra){
+        if($coincidencia = strrpos($palabra,"EXCELENTE SERVICIO")){
+          $conversacion->sumarPuntaje(100);
+          $terminarCalificar = true;
+        }
+        if(!$terminarCalificar){
+          if( $coincidencia = strrpos($palabra,"Gracias") || $coincidencia = strrpos($palabra,"buena atención") ||
+          $coincidencia = strrpos($palabra,"Muchas Gracias")){
+          $conversacion->sumarPuntaje(10);
+          }
+        }
+     
+      }
 
-         var_dump($conversaciones);
-
-         echo($conversaciones);
-        
+ //-----------Tiempo en los mensajes-------------------------------------------------------------   
+    if(!$terminarCalificar){
+      $HoraInicial = substr($stringMensajes,0,8);
+      $HoraFinal = substr($palabra,0,8);
+      list($hora,$min,$seg) = explode(":",$HoraInicial);
+      list($hora2,$min2,$seg2) = explode(":",$HoraFinal);
+      $resultadomin = $min2 - $min;
+      $resultado =$seg2 - $seg;
+      if($resultadomin > 0){
+        $resultado = $resultado + $resultadomin * 60;
+      }
+      echo $resultado . "<br>";
+      if($resultado <= 60){
+        $conversacion->sumarPuntaje(50);
+      }else{
+        $conversacion->sumarPuntaje(25);
+      }
     }
-
-
+      //var_dump($conversacion);
+      calcularEstrellas($conversacion);
+    }
+  }
+ 
 }
+function calcularEstrellas($conversacion){
+ $puntaje = $conversacion->getPuntaje();
+  if($puntaje < 0){
+    $conversacion->agregarEstrellas(0);
+  }
+  else{
+      if($puntaje < 25){
+        $conversacion->agregarEstrellas(1);
+      }
+      else{
+          if($puntaje >= 25 &&  $puntaje < 50){
+            $conversacion->agregarEstrellas(2);
+          }
+          else{
+            if($puntaje >= 50 && $puntaje < 75){
+              $conversacion->agregarEstrellas(3);
+            }
+            else{
+              if($puntaje >=75 && $puntaje <90){
+                $conversacion->agregarEstrellas(4);
+              }
+              else{
+                $conversacion->agregarEstrellas(5);
+              }
+            }
+          }
+      }
+  } 
+  var_dump($conversacion);
+  $array = array();
+  foreach($Listaconversaciones as $posicion =>$conversacion){
+    $arr = array('id' => $conversacion->getId(), 'puntaje' => "subido con exito",'estrellas' => );
+    array_push($array,$arr);
+  }
+  echo json_encode($array); 
+  
+}
+
+
+$nueva = new Calificacion();
+$nueva->calificar();
+
--- Backend/modelo/conversacion.php
+++ Backend/modelo/conversacion.php
@@ -1,82 +1,88 @@
 <?php
 
 class Conversacion {
-    private $id; // id de la conversacion
-    private $cliente;
-    private $asesor;
-    private $mensajes;
-    private $listaConversaciones;
-   
 
+   private $mensajes;
+   private $id;
+   private $puntaje;
+   private $estrellas;
 
-    function __construct($id){
-        $this->id = $id;
-        $this->extraerConversacion();
-        $this->mensajes = array();
-        $this->listaConversaciones = array();
+   function __construct(){
+       $this->mensajes = array();
+       $this->puntaje = 0;
+       $this->estrellas = 0;
 
-    }
-
-    function getId(){
-        return $id;
-    }
-
-    function getCliente(){
-        return $cliente;
-    }
-
-    function getAsesor(){
-        return $asesor;
-    }
-
-    function setId($id){
-        $this->id =$id;
-    }
-
-    function setCliente($cliente){
-        $this->cliente =$cliente;
-    }
-
-    function setAsesor($asesor){
-        $this->asesor =$asesor;
-    }
-
-    function agregarMensaje($linea){
-        array_push($mensajes,$linea);
-    }
-
-    function agregarConversacion($conversacion){
-        array_push($listaConversaciones,$conversacion);
-    }
-
-
-
-
-  static function extraerConversacion(){
     
-    //lee el archivo solo para lectura      
-    $fp = fopen("../conversacion/conversacion.txt","rb");
-    $conversacion = new Conversacion(); 
-    $listaConversaciones = array();
+   }
 
-    //conversacion -> agregarMensaje($linea), array push 
-    while (!feof($fp)){
-        $linea = fgets($fp);
-        $linea = trim($linea);
-        $conversacion -> agregarMensaje($linea);
-        echo $linea . "<br>" ;
-        //. strlen(trim($linea)),  strlen logitud de la cadena, trim borra las cadenas vacias al principio y al final
+   function agregarMensaje($linea){
+       array_push($this->mensajes,$linea);
+       
+   }
 
-        if($linea == ""){
+   function obtenerMensajes(){
+        return $this->mensajes;
+   }
 
-            echo " aqui empieza una nueva conversacion <br>"; 
-            //$listaConversaciones debo agregarle la $conversacion
-            $conversacion = new Conversacion(); 
-        }
+   function getId(){
+       return $this->id;
+
+   }
+
+   function setId($id){
+        $this->id=$id;
+   }
+
+   function getPuntaje(){
+    return $this->puntaje;
     }
-    return $listaConversaciones;
 
-    fclose($fp);
+    function sumarPuntaje($puntaje){
+     $this->puntaje += $puntaje;
+    }
+
+   function agregarEstrellas($estrella){
+        $this->estrellas = $estrella;
+    }
+    function getEstrellas(){
+        return $this->estrellas;
+    }
+    function extraerConversacion(){
+    
+        //lee el archivo solo para lectura      
+        $fp = fopen("../conversacion/conversacion.txt","rb");
+        $conversacion = new Conversacion(); 
+        $numeroLinea = 0;
+        $listaConversaciones = array();
+        while (!feof($fp)){
+            $linea = fgets($fp);
+            $linea = trim($linea);
+            if($numeroLinea == 0){
+                $conversacion->setId($linea);
+            }
+            else{
+                if($linea != ""){
+                    $conversacion->agregarMensaje($linea);
+                }
+            }
+            $numeroLinea++;
+            //. strlen(trim($linea)),  strlen logitud de la cadena, trim borra las cadenas vacias al principio y al final
+
+            if($linea == ""){
+               // echo " aqui empieza una nueva conversacion <br>"; 
+                //$listaConversaciones debo agregarle la $conversacion
+                array_push($listaConversaciones,$conversacion);
+                $conversacion = new Conversacion(); 
+                $numeroLinea = 0;
+            }
+        }
+        array_push($listaConversaciones,$conversacion);
+        return $listaConversaciones;
+
+         fclose($fp);
   
    }
 }
+
+
+
